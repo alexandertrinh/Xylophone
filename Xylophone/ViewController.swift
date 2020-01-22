b004e6f35f56c8143cd4316f33921d7bbdc0429f
alexandertrinh <alexander9821@gmail.com>
2020-01-21 21:02:03 -0800

    First commit for Xylophone app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,36 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
+    var buttonName: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    func playSound(letter: String) {
+        let url = Bundle.main.url(forResource: letter, withExtension: "wav")
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try! AVAudioPlayer(contentsOf: url!, fileTypeHint: AVFileType.wav.rawValue)
+            
+            guard let player = player else { return }
+            
+            player.play()
+            
+        } catch let error as NSError{
+            print(error.localizedDescription)
+        }
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        buttonName = sender.currentTitle
+        playSound(letter: buttonName!)
+    }
 }
 
